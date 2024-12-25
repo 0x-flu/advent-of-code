31017b00c9fbf2fd04b84baaf549b2d6a84c2904
0x_flu <0xfluen@gmail.com>
2024-12-25 23:02:58 +0100

    Day 02 setup for cpp

---
+++ 2024/day02/cpp/day02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "parts.h"
+
+using namespace std;
+
+int main() {
+    cout << "Day 02 Part I : ";
+    parts::part1(); 
+
+    cout << "Day 02 Part II: ";
+    parts::part2(); 
+
+    return 0;
+}
+
---
+++ 2024/day02/cpp/parts.h
@@ -0,0 +1,17 @@
+#ifndef PARTS_H
+#define PARTS_H
+
+#include <iostream> 
+
+namespace parts {
+    void part1() {
+        std::cout << "unknown yet" << std::endl;
+    }
+
+    void part2() {
+        std::cout << "unknown yet" << std::endl;
+    }
+}
+
+#endif 
+
